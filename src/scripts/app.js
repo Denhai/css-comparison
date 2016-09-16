af4474cfacb4e1ea130391401da616d31733743a
Hayden <haybartlett@gmail.com>
2016-09-17 02:01:32 +0930

    iframe sizing

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,22 @@
 
 $(function () {
     var tabs = $('.tabs')
-    var lastOpacity = 1;
-
+    var lastOpacity = 1
     $(window).scroll(function () {
         var opacity = 1 - $(this).scrollTop() / 50
         opacity = Math.round(opacity)
         if (lastOpacity > 0 || opacity > 0)
-            tabs.css({ 'opacity': opacity });
+            tabs.css({ 'opacity': opacity })
         lastOpacity = opacity
     });
 
-    $("iframe").on('load', function () {
-        this.style.height = this.contentWindow.document.body.scrollHeight + 'px';
+    function resize() {
+        this.style.height = this.contentWindow.document.body.scrollHeight + 5 + 'px'
+    }
+    // increase the size of the iframe so that there are no scrollbars
+    $("iframe").load(function() {
+        resize.apply(this)
+        setTimeout(resize.bind(this), 1000)
     })
 })
 
